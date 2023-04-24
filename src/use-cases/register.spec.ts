54947b99e20234a1741816a46284ee4e57579cef
gregsvieira <vieiragregory42@gmail.com>
2023-04-23 22:50:22 -0300

    test/register-user added vitest added test of usecase register: is working registration?; password is hashed?; can't create user if email already in use? create repository in memory to do test without connections on database

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,62 @@
+import { expect, test, describe, it } from 'vitest';
+import { RegisterUseCase } from './register';
+import { compare } from 'bcryptjs';
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
+import { rejects } from 'assert';
+import { UserAlreadyExistsError } from './errors/user-already-exists-error';
+
+describe('Register Use Case', () => {
+	it('Should be to register', async ()=> {
+		const usersRepository = new InMemoryUsersRepository();
+
+		const registerUseCase = new RegisterUseCase(usersRepository);
+
+		const { user } = await registerUseCase.execute({
+			name: 'John Dee',
+			email: 'johndee@email.com',
+			password: '123456',
+		});
+
+		expect(user.id).toEqual(expect.any(String));
+	});
+
+	it('Should be hash user password upon registration', async ()=> {
+		const usersRepository = new InMemoryUsersRepository();
+
+		const registerUseCase = new RegisterUseCase(usersRepository);
+
+		const { user } = await registerUseCase.execute({
+			name: 'John Dee',
+			email: 'johndee@email.com',
+			password: '123456',
+		});
+
+		const isPasswordCorrectlyHashed = await compare('123456',
+			user.password_hash
+		);
+
+		expect(isPasswordCorrectlyHashed).toBe(true);
+	});
+
+	it('Should not be able to register with same email twice', async () => {
+		const usersRepository = new InMemoryUsersRepository();
+		const registerUseCase = new RegisterUseCase(usersRepository);
+
+		const email =	'johndee@email.com';
+
+		await registerUseCase.execute({
+			name: 'John Dee',
+			email,
+			password: '123456',
+		});
+
+		await expect(()=> 
+			registerUseCase.execute({
+				name: 'John Dee',
+				email,
+				password: '123456',
+			})
+		).rejects.toBeInstanceOf(UserAlreadyExistsError);
+
+	});
+});
